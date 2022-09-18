6b6aba9770eb06e51ac5d6186772b73449d9ecf4
Harut101 <arutaydinyan@gmail.com>
2022-09-19 03:03:51 +0400

    added headers

--- src/api/bitlyApi.js
+++ src/api/bitlyApi.js
@@ -8,6 +8,9 @@ export function getAccessToken() {
       controller = new AbortController();
       return http.post(`https://bitly.com/oauth/access_token`, null, {
         signal: controller.signal,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
         params: {
           client_id: process.env.REACT_APP_BITLY_CLIENT_ID,
           client_secret: process.env.REACT_APP_BITLY_CLIENT_SECRET,
