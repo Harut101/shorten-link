4fb07e7367ee95572ae11f1aa159ce8f0e1b07fa
Harut101 <arutaydinyan@gmail.com>
2022-09-20 13:10:06 +0400

    added user auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import Header from "./components/header/Header";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Container from "@mui/material/Container";
+import Header from "./components/header/Header";
 import useAppStyles from "./styles/app-styles";
 import Pages from "./pages";
-import useAuth from "./hooks/useAuth";
+import { getUserApi } from "./api/userApi";
+import { authorize } from "./store/reducers/userReducer";
 import "./App.css";
+
+const getUser = getUserApi();
 
 function App() {
   const { app, appBody } = useAppStyles();
-  const isAuth = useAuth();
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function get() {
+      const { data: userData } = await getUser.call();
+      dispatch(authorize(userData));
+    }
+
+    !user.loggedIn && get();
+
+    return () => getUser.cancel();
+  }, [user, dispatch]);
 
   return (
     <div className={app}>
-      <Header isAuth={isAuth} />
+      <Header isAuth={user.loggedIn} />
       <Container className={appBody}>
         <Pages />
       </Container>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 const SignIn = lazy(() => import("./signIn/SignIn"));
-const OauthRedirect = lazy(() => import("./oauthRedirect/OauthRedirect"));
 const LinkEditor = lazy(() => import("./link-editor/LinkEditor"));
 
 function Pages() {
@@ -10,7 +9,6 @@ function Pages() {
       <Routes>
         <Route path="/" element={<LinkEditor />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/redirect" element={<OauthRedirect />} />
       </Routes>
     </Suspense>
   );
