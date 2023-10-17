23e1910ec9aa7e860e5a4d3bb8f2fa6889da6f0d
Harut101 <arutaydinyan@gmail.com>
2023-10-17 18:35:54 +0400

    fix

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,103 +2,111 @@ import { useState, useCallback, useRef } from "react";
 import validateForm from "../services/formValidator";
 
 function isEmpty(obj) {
-    return Object.keys(obj).length === 0;
+  return Object.keys(obj).length === 0;
 }
 
 const useForm = (schema, submitHandler) => {
-    const [formFields, setFormFields] = useState({ ...schema.initialValues });
-    const [submitted, setSubmitted] = useState(false);
-    const [errors, setErrors] = useState({});
-    const fieldRefs = useRef({});
+  const [formFields, setFormFields] = useState({ ...schema.initialValues });
+  const [submitted, setSubmitted] = useState(false);
+  const [errors, setErrors] = useState({});
+  const fieldRefs = useRef({});
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        setSubmitted(true);
+    setSubmitted(true);
 
-        let errors = validateForm(formFields, schema.validators);
+    let errors = validateForm(formFields, schema.validators);
 
-        setErrors(errors);
+    setErrors(errors);
 
-        if (isEmpty(errors)) {
-            submitHandler(formFields);
+    if (isEmpty(errors)) {
+      submitHandler(formFields);
+    }
+  };
+
+  const onChange = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const { name, value } = e.target;
+
+      if (submitted) {
+        const validated = validateForm(
+          formFields,
+          { [name]: schema.validators[name] },
+          value
+        );
+        const errorsClone = { ...errors };
+
+        if (isEmpty(validated)) {
+          delete errorsClone[name];
         }
-    };
 
-    const onChange = useCallback(
-        (e) => {
-            e.preventDefault();
+        setErrors({ ...errorsClone, ...validated });
+      }
 
-            const { name, value } = e.target;
+      setFormFields({ ...formFields, [name]: value });
+    },
+    [formFields, submitted, errors, schema]
+  );
 
-            if (submitted) {
-                const validated = validateForm(formFields, { [name]: schema.validators[name] }, value);
-                const errorsClone = { ...errors };
+  const setValue = useCallback(
+    (name, value) => {
+      if (fieldRefs.current[`${name}`].current) {
+        fieldRefs.current[`${name}`].current.value = value;
+      }
+      setFormFields({ ...formFields, [name]: value });
+    },
+    [formFields, fieldRefs]
+  );
 
-                if (isEmpty(validated)) {
-                    delete errorsClone[name];
-                }
+  const getValue = useCallback((name) => formFields[name], [formFields]);
 
-                setErrors({ ...errorsClone, ...validated });
-            }
+  const setError = useCallback(
+    (name, message) => setErrors({ ...errors, [name]: message }),
+    [errors]
+  );
 
-            setFormFields({ ...formFields, [name]: value });
-        },
-        [formFields, submitted, errors, schema]
-    );
+  const reset = useCallback(
+    (name = null) => {
+      if (name) {
+        setFormFields({ ...formFields, [name]: schema.initialValues[name] });
+        fieldRefs.current[`${name}`].current.value = schema.initialValues[name];
+      } else {
+        setFormFields({ ...schema.initialValues });
+        for (const refName in fieldRefs.current) {
+          fieldRefs.current[refName].current.value =
+            schema.initialValues[refName];
+        }
+      }
+    },
+    [formFields, fieldRefs, schema.initialValues]
+  );
 
-    const setValue = useCallback(
-        (name, value) => {
-            if (fieldRefs.current[`${name}`].current) {
-                fieldRefs.current[`${name}`].current.value = value;
-            }
-            setFormFields({ ...formFields, [name]: value });
-        },
-        [formFields, fieldRefs]
-    );
+  const register = (name) => {
+    /* eslint-disable */
+    const fieldRef = useRef();
 
-    const getValue = useCallback((name) => formFields[name], [formFields]);
-
-    const setError = useCallback((name, message) => setErrors({ ...errors, [name]: message }), [errors]);
-
-    const reset = useCallback(
-        (name = null) => {
-            if (name) {
-                setFormFields({ ...formFields, [name]: schema.initialValues[name] });
-                fieldRefs.current[`${name}`].current.value = schema.initialValues[name];
-            } else {
-                setFormFields({ ...schema.initialValues });
-                for (const refName in fieldRefs.current) {
-                    fieldRefs.current[refName].current.value = schema.initialValues[refName];
-                }
-            }
-        },
-        [formFields, fieldRefs, schema.initialValues]
-    );
-
-    const register = (name) => {
-        /* eslint-disable */
-        const fieldRef = useRef();
-
-        fieldRefs.current[`${name}`] = fieldRef;
-
-        return {
-            name,
-            ref: fieldRef,
-            onChange,
-        };
-    };
+    fieldRefs.current[`${name}`] = fieldRef;
 
     return {
-        errors,
-        submitted,
-        register,
-        onSubmit,
-        setValue,
-        getValue,
-        setError,
-        reset,
+      name,
+      ref: fieldRef,
+      onChange,
     };
+  };
+
+  return {
+    errors,
+    submitted,
+    register,
+    onSubmit,
+    setValue,
+    getValue,
+    setError,
+    reset,
+  };
 };
 
 export default useForm;
